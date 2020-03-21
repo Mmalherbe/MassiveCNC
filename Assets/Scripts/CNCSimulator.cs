d996d352899b3f8bb8270929da8004fef340460c
Max Malherbe <mprmalherbe@gmail.com>
2020-03-21 19:50:18 +0100

    init

---
+++ Assets/Scripts/CNCSimulator.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GCodeNet;
+using System.IO;
+
+public class CNCSimulator : MonoBehaviour
+{
+    
+    [SerializeField] private RectTransform CNCHead = null;
+    [Header("Max size. Do not change unless you are 100% sure. Values in Meters.")]
+    [SerializeField]private Vector2 CNCSize = new Vector2(10, 5);
+    [SerializeField] RectTransform SizeForSimOnly = null;
+    [Header("Speed used for movement.")]
+    [SerializeField] private float speed = 1f;
+    float timeOfTravel = 5; //time after object reach a target place 
+    float currentTime = 0; // actual floting time 
+    float normalizedValue;
+    [SerializeField] private List<Vector3> parabolaPoints = new List<Vector3>();
+    [SerializeField] private bool _Moving;
+    [Header("!Do not adjust these positions!")]
+    [SerializeField] private Vector2 endPosition;
+    [SerializeField] private Vector2 startPositionGcodeLine;
+    [SerializeField] private Vector2 currentPositionOfHead;
+
+    [SerializeField] private List<string> gcodeLines;
+    [SerializeField] private int lineCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ReadFileAndStartSimulating()
+    {
+        using (StreamReader sr = new StreamReader("Assets/example.cnc")) // read file , 
+        {
+            // Read the stream to a string, and write the string to the console.
+             while(sr.Peek() >= 0)
+            {
+                gcodeLines.Add(sr.ReadLine());
+            }
+        }
+
+        StartSimulation();
+
+    }
+    private void StartSimulation()
+    {
+
+        CreateCommandFromGCodes(gcodelines:gcodeLines);
+    }
+
+
+    public void CreateCommandFromGCodes(List<string> gcodelines)
+    {
+        for (lineCounter = 0; lineCounter < gcodelines.Count;)
+        {
+            var cmd = Command.Parse(gcodelines[lineCounter]);
+            Debug.Log(cmd.CommandType); //Output: "G"
+            Debug.Log(cmd.CommandSubType); //Output: "1"
+            Debug.Log(cmd.GetParameterValue(ParameterType.X)); //Output: "10"
+            Debug.Log(cmd.GetParameterValue(ParameterType.Y)); //Output: "20"
+            /// 
+            /// G code explained
+            /// 
+            /// G1 : Linear movement ( go to point x , y ) Relative!
+            /// G2 : Arc movement clockwise 
+            /// 
+
+
+            if (cmd.CommandType == CommandType.G)  // cnc402 uses G1 G2 G3 
+            {
+                if (cmd.CommandSubType == 1) // Linear movement
+                {
+                    string x = cmd.GetParameterValue(ParameterType.X).ToString();
+                    string y = cmd.GetParameterValue(ParameterType.Y).ToString();
+                    float smoothFactorX = SizeForSimOnly.sizeDelta.x / CNCSize.x; // Make the size of the window for sim unimportant
+                    float smoothFactorY = SizeForSimOnly.sizeDelta.y / CNCSize.y; // Make the size of the window for sim unimportant
+                    endPosition = new Vector2(x != null ? (startPositionGcodeLine.x + int.Parse(x) * smoothFactorX) : startPositionGcodeLine.x * smoothFactorX, y != null ? (startPositionGcodeLine.y + int.Parse(y)) * smoothFactorY : startPositionGcodeLine.y * smoothFactorY);
+                    
+
+
+                }
+                else if (cmd.CommandSubType == 2) // example line G03 X0.9926 Y1.2734 I-0.1125 J0.0316 
+                {
+                    ///<summary>
+                    /// G03 / G02 X.. Y.. I and J are the difference from the start point to the center of the arc.
+                    /// I is on X axis, J is on Y axis.
+                    /// Great tutorial and example on how to write G02 / G03 https://www.youtube.com/watch?v=gXSlyxMiZ6s 
+                    /// </summary>
+
+
+                }
+
+            }
+        }
+
+    }
+
+}
