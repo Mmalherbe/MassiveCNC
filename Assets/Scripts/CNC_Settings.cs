b18ff6e29d1b35c98c79c8c0e2b80d18bbff9993
windowsPC Max <mprmalherbe@gmail.com>
2020-03-25 20:51:08 +0100

    start rewrite new project

---
+++ Assets/Scripts/CNC_Settings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CNC_Settings 
+{
+    public static int importBezierLineSegmentsCnt;
+    public static bool importResizeSVG;
+    public static float importSVGMaxSize;
+    public static bool importSVGPathClose;
+    public static bool importSVGToMM;
+    public static bool importSVGNodesOnly;
+    public static bool importSVGGroups;
+}
---
+++ Assets/Scripts/cncLogger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class cncLogger
+{
+    // Start is called before the first frame update
+   public static void Log(object txt)
+    {
+        Debug.Log(txt);
+
+
+    }
+}
