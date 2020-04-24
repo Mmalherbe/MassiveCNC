bbf984d2a0e8942a50636f290b853b7e8b0bcbf9
windowsPC Max <mprmalherbe@gmail.com>
2020-04-24 08:50:09 +0200

    svg work

---
+++ Assets/Scripts/classes/AISvg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.classes
+{
+    public class AISvg
+    {
+        public string id;
+        public string dataname;
+        public string xmlns;
+        public Dictionary<string, string> defs;
+        public List<svgPath> paths;
+    }
+    public class svgPath
+    {
+        public string d;
+        public string Class;
+        public List<PathCommand> commands;
+    }
+
+    public class PathCommand
+    {
+        public string commandType;
+        public float X;
+        public float Y;
+
+    }
+
+}
---
+++ Assets/svgToPath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using System.Xml.Serialization;
+
+public class svgToPath : MonoBehaviour
+{
+
+    public void Parse(string urlToPath)
+    {
+        if(!File.Exists(urlToPath) || Path.GetExtension(urlToPath).ToUpper() != "SVG")
+        {
+            return;
+
+        }
+
+
+
+    }
+
+}
